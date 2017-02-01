8086b8f08fb0e04a5358c0f865f0583af629e41e
simchanu29 <simon.chanu@ensta-bretagne.org>
2017-02-01 11:44:00 +0100

    Ajout autologin et la configuration de base pour ubuntu arm

--- INSTALL-ALL-Pi2-Ubuntu14.04.2.sh
+++ INSTALL-ALL-Pi2-Ubuntu14.04.2.sh
@@ -20,7 +20,10 @@
 
 #    Manual steps
 # 1.Setup network connection (Ethernet REQUIRED)
-# 2.Launch this script
+# 2.Move this script on a USB key on the freshly installed raspberry
+# 3.Launch this script
+
+# === PART I ===
 
 #    Resize file system with parted
 parted /dev/mmcblk0 -s p
@@ -28,6 +31,9 @@ parted /dev/mmcblk0 -s rm 2
 parted /dev/mmcblk0 -s mkpart p ext4 1 -1
 parted /dev/mmcblk0 -s p
 # REBOOT REQUIRED
+
+# === PART II ===
+
 resize2fs /dev/mmcblk0p2
 
 #    Init
@@ -41,26 +47,41 @@ export alias agyi="sudo apt-get -y install"
 export alias acs="sudo apt-cache search"
 source ~/.bashrc
 
-#    raspi-config est absent
+#    raspi-config est absent, configuration avec https://wiki.ubuntu.com/ARM/RaspberryPi
+#    Install swap partition
+# The following packages should be installed : 
+# dphys-swapfile
+apt-get -y install dphys-swapfile
+# REBOOT REQUIRED
 
-#    Enable autologin
-#sudo mkdir /etc/lightdm/lightdm.conf.d
-#sudo perl -i -pe 's/.*/autologin-user=simon/ if $.==10' /etc/lightdm/lightdm.conf.d/50-myconfig.conf
+# === PART III ===
+
+#    Install wifi drivers
+# The following packages should be installed : 
+#  
+apt-get -y install linux-firmware
+#    Install ssh
+# The following packages should be installed : 
+# ncurses-term openssh-client openssh-sftp-server ssh-import-id
+apt-get -y install openssh-server
 
 #    Install LXDE
 # LOADS of packages should be installed
 agyi lubuntu-desktop
 dpkg --configure -a
 
+#    Enable autologin
+perl -i -pe 's/.*/autologin=ubuntu/ if $.==3' /etc/lxdm/default.conf
+
 #    git
 # The following packages should be installed :
 # git git-man liberror-perl
-agyi git
+apt-get -y install  git
 
 #    zsh
 # The following packages should be installed : 
 # zsh zsh-common
-agyi zsh
+apt-get -y install  zsh
 #sudo chsh -s $(which zsh) 
 	# Set up zsh as default shell
     # Be careful : you will have trouble to get back your bash console if you uncomment this.
@@ -71,8 +92,10 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 	# ===== PASSWORD =====
 	# I'm not sure how the following will work so be careful.
 exit
-# theme and plugin configurations
-perl -i -pe 's/.*/ZSH_THEME="agnoster"/ if $.==10' /tmp/file.txt
+# Theme and plugin configurations
+perl -i -pe 's/.*/ZSH_THEME="agnoster"/ if $.==10' ~/.zshrc
+# Fonts
+git clone ...
 cd fonts
 ./install.sh
 	# To make the agnoster theme properly work you have to change the fonts used by your terminal
@@ -84,7 +107,7 @@ cd fonts
 # libgnome2-bin libgnome2-common libgnomecanvas2-0 libgnomecanvas2-common libgnomeui-0 libgnomeui-common
 # libgnomevfs2-0 libgnomevfs2-common libidl-2-0 libkeybinder0 liborbit-2-0 liborbit2 python-gconf
 # python-gnome2 python-keybinder python-notify python-pyorbit python-vte terminator
-agyi terminator 
+apt-get -y install  terminator 
 
 #    ROS-indigo-desktop
 # enable universe and multiverse repositories (not useful for ROS)
@@ -96,8 +119,8 @@ sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sour
 	# This is only for Ubuntu 14.04 so it might cause some problems
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 	# add (adress):80 if there is some problems
-agu
-agyi ros-indigo-desktop 
+apt-get update
+apt-get -y install  ros-indigo-desktop 
 	# test 1 : echoué sur Mate car broken packages.
 	# test 2 : échoué sur Ubuntu 16.04 car broken packages
 #agyi ros-indigo-ros-base
