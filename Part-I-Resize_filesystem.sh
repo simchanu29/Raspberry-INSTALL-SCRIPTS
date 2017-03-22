982ed5dcecaef690526c48dbf3603708a2e5fa84
simchanu29 <simon.chanu@ensta-bretagne.org>
2017-03-22 16:06:44 +0100

    partitions bugs 2

--- Part-I-Resize_filesystem.sh
+++ Part-I-Resize_filesystem.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-# to create the partitions programatically (rather than manually)
-# we're going to simulate the manual input to fdisk
-# The sed script strips off all the comments so that we can
-# document what we're doing in-line with the actual commands
-# Note that a blank line (commented as "defualt" will send a empty
-# line terminated with a newline to take the fdisk default.
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-   p # print
-   d # delete partition
-   2 # partition number 2
-   n # new partition
-   p # primary partition
-   2 # partition number 2
-    # default
-    # default
-   p # print
-   w # write changes
-   q # quit
-EOF
+# \n is used prevent multiple echo
+echo "p" && echo "d" && echo "2" && echo "n" && echo "p" && echo "2" && echo " " && echo " " && echo "p" && echo "w" && echo "q" | fdisk /dev/mmcblk0
+#   p # print
+#   d # delete partition
+#   2 # partition number 2
+#   n # new partition
+#   p # primary partition
+#   2 # partition number 2
+#    # default
+#    # default
+#   p # print
+#   w # write changes
+#   q # quit
 
 # REBOOT REQUIRED
